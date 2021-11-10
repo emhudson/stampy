053000ad9532fb464ea458698cae3f9d2f399f87
Matt Wilkins <matt@galacticpolymath.com>
2021-11-09 18:26:10 -0600

    decent looking proof of concept stamp

--- PlotStamps.R
+++ PlotStamps.R
@@ -1,13 +1,12 @@
 #test dataframe
 
+
 #plot a stamp
-stampVal="30"
-stampWidth=.8
-stampHeight=.8
+plotStamp<-function(stampVal=30,stampWidth=.8,stampHeight=.8){
 nScallops=15
 nudgeEdge=0.009
 
-  scallop_radius=(1/nScallops)/2
+scallop_radius=(1/nScallops)/2
   x1=scallop_radius
   x=x1+((1:nScallops-1)*(scallop_radius*2))
   y=scallop_radius+((1:nScallops-1)*(scallop_radius*2))
@@ -16,27 +15,44 @@ scallopCoords<-data.frame(x=c(x,x,rep(0,length(x)),rep(1,length(x))),
                           r=scallop_radius)  
 vp_stamp<-grid::viewport(width=stampWidth,height=stampHeight,clip="on")
 
+scallops<-  grid::circleGrob(
+    x = scallopCoords$x,
+    y = scallopCoords$y,
+    r = scallopCoords$r,
+    gp = grid::gpar(fill = "black", col = "gray10"),
+    vp = vp_stamp
+)
+
 
 grid::grid.newpage()
-grid::grid.draw(grid::rectGrob(name = "stamp",
-                        x = 0.5, y = 0.5, width=stampWidth-nudgeEdge,
-                        height=stampHeight-nudgeEdge,
-                        gp = grid::gpar(col = "gray10",fill="gray"),
-                        vp=grid::viewport(mask=scallops)))
-grid::grid.draw(grid::rectGrob(name = "stamp",
+# grid::grid.draw(
+  stamp_grob <- grid::gTree(children=grid::gList(
+    grid::rectGrob(name = "outerRec",
+                        x = 0.5, y = 0.5, width=stampWidth,
+                        height=stampHeight,
+                        gp = grid::gpar(col = "gray10",fill="gray")),
+    grid::rectGrob(name = "innerRec",
                         x = 0.5, y = 0.5, width=.6,
                         height=.6,
-                        gp = grid::gpar(col = "gray30",fill="gray80")))
-
-scallops<-grid::circleGrob(
-  x = scallopCoords$x,
-  y = scallopCoords$y,
-  r= scallopCoords$r,
-  gp=grid::gpar(fill="white",col="gray10"),
-  vp=vp_stamp
-)
-grid::grid.draw(scallops)
+                        gp = grid::gpar(col = "gray30",fill="gray80")),
+  grid::circleGrob(name = "scallops",
+    x = scallopCoords$x,
+    y = scallopCoords$y,
+    r = scallopCoords$r,
+    gp = grid::gpar(fill = "white", col = "gray10"),
+    vp = vp_stamp
+    )
+))
+grid::grid.clip(x = 0.5, y = 0.5, width=stampWidth,
+                        height=stampHeight)
+grid::grid.draw(stamp_grob)
 grid::grid.text(label = stampVal,x=0.5,y=0.5,gp = grid::gpar(fontsize=35))
+
+
+}
+
+plotStamp(40,0.6,0.6)
+
 
 
 
